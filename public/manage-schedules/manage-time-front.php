552bf1556fc939e70d25aea3079c32db36d13b69
Aaraharoon <aarathilfarharoon@gmail.com>
2024-11-17 05:57:26 +0530

    Update manage-time-front.php

--- public/manage-schedules/manage-time-front.php
+++ public/manage-schedules/manage-time-front.php
@@ -29,6 +29,7 @@ unset($_SESSION["success_message"], $_SESSION["errors"], $_SESSION["form_data"])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Restaurant Hours</title>
+    <link rel="stylesheet" href="/assets/css/manage-hours.css">
 </head>
 
 <div>
