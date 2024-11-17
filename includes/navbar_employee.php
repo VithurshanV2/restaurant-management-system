648137ea64a6c7f060fe8411bdc9e89ba5f387f1
Vithurshan Pradeepkumar <vithurshanpradeep@gmail.com>
2024-11-17 22:02:11 +0530

    Fixed minor issues in css for navbar and footer

--- includes/navbar_employee.php
+++ includes/navbar_employee.php
@@ -1,10 +1,12 @@
 <link rel="stylesheet" href="/assets/css/navbar-footer.css">
-
-<nav>
-    <ul>
-        <li><a href="/public/employee-reservations/employee-reservations-front.php">Reservations</a></li>
-        <li><a href="/public/employee-schedules/employee-schedules-front.php">Schedules</a></li>
-        <li><a href="/public/employee-shifts/employee-shifts-front.php">Shifts</a></li>
-        <li><a href="/public/login/logout.php">Logout</a></li>
-    </ul>
-</nav>
+<header>
+    <h1>
+        Dashboard
+    </h1>
+    <nav>
+        <a href="/public/employee-reservations/employee-reservations-front.php">Reservations</a>
+        <a href="/public/employee-schedules/employee-schedules-front.php">Schedules</a>
+        <a href="/public/employee-shifts/employee-shifts-front.php">Shifts</a>
+        <a href="/public/login/logout.php">Logout</a>
+    </nav>
+</header>
