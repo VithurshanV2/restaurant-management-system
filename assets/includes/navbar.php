e4a1a8b8ba81415b9717ee75eb746f39a99404fe
Aaraharoon <aarathilfarharoon@gmail.com>
2024-11-17 20:42:05 +0530

    Update navbar_manager.php

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -1,1 +1,15 @@
-
+<!-- Navigation Section -->
+<nav>
+    <ul>
+        <li><a href="/public/home/home.php">Home</a></li>
+        <li><a href="/public/about/about.php">About Us</a></li>
+        <li><a href="/public/menu/menu.php">Menu</a></li>
+        <li><a href="/public/feedback/feedback.php">Feedback</a></li>
+        <li><a href="/public/manage-reservations/manage-reservations.php">Manage Reservations</a></li>
+        <li><a href="/public/manage-schedules/manage-time-front.php">Manage Schedules</a></li>
+        <li><a href="/public/manage-tables/manage-tables-front.php">Manage Tables</a></li>
+        <li><a href="/public/manage-shifts/manage-shifts-front.php">Manage Shifts</a></li>
+        <li><a href="/public/gallery/gallery.php">Gallery</a></li>
+        <li><a href="/public/login/logout.php">Logout</a></li>
+    </ul>
+</nav>
