7474d3e3e8d908ba73b4a124c4e50c7099f73f09
Aaraharoon <aarathilfarharoon@gmail.com>
2024-11-17 20:05:21 +0530

    Update employee-reservations-front.php

--- public/employee-reservations/employee-reservations-front.php
+++ public/employee-reservations/employee-reservations-front.php
@@ -41,6 +41,9 @@ unset($_SESSION["success_message"], $_SESSION["errors"]);
 </head>
 
 <body>
+   <!-- Include Navigation Bar --> 
+   <?php include 'navbar.php'; ?>
+    
     <div>
         <h2>Reservations for <?php echo htmlspecialchars($selected_day ?: 'Selected Day'); ?></h2>
         <form action="employee-reservations-back.php" method="post">
@@ -87,6 +90,10 @@ unset($_SESSION["success_message"], $_SESSION["errors"]);
             <?php endforeach; ?>
         </div>
     <?php endif; ?>
+
+    <!-- Include Footer -->
+    <?php include 'footer.php'; ?>
+
 </body>
 
 </html>
