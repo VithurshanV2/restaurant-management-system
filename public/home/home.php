a23ea2c3a15a75bff14e87ef334955d8b7cabc1e
Vithurshan Pradeepkumar <vithurshanpradeep@gmail.com>
2024-11-26 12:24:01 +0530

    Added map view

--- public/home/home.php
+++ public/home/home.php
@@ -110,6 +110,11 @@
     </section>
 
 
+    <!-- google map view embedded -->
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d247.61186169256146!2d79.90312124966417!3d6.795412548542516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2slk!4v1732603671726!5m2!1sen!2slk" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
     <!-- Footer -->
     <footer>
         <p>Follow us on social media!</p>
